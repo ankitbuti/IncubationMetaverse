da216e32f2c7e392a159c57c48dc33cd2f8dab44
KingLovedeep <lovedeep@startuped.ai>
2025-04-09 17:41:33 +0530

    new build pushed

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Startuped AI-IncubationMetaverse-0.0.1";
+const contentToCache = [
+    "Build/new build 1.loader.js",
+    "Build/new build 1.framework.js.unityweb",
+    "Build/new build 1.data.unityweb",
+    "Build/new build 1.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
